367ddc34a600b2f6d079d6ef83428484fe440ebb
Pedro <palves.ct@petros.com>
2025-01-21 12:44:42 -0300

    Criação model

---
+++ src/model/CartaoModel.java
@@ -0,0 +1,73 @@
+package model;
+
+public class CartaoModel {
+    private int id;
+    private String numero;
+    private String nomeTitular;
+    private String dataExpiracao;
+    private int cvv;
+
+    // Construtor vazio
+    public CartaoModel() {}
+
+    // Construtor com parâmetros
+    public CartaoModel(int id, String numero, String nomeTitular, String dataExpiracao, int cvv) {
+        this.id = id;
+        this.numero = numero;
+        this.nomeTitular = nomeTitular;
+        this.dataExpiracao = dataExpiracao;
+        this.cvv = cvv;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public String getDataExpiracao() {
+        return dataExpiracao;
+    }
+
+    public void setDataExpiracao(String dataExpiracao) {
+        this.dataExpiracao = dataExpiracao;
+    }
+
+    public int getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(int cvv) {
+        this.cvv = cvv;
+    }
+
+    @Override
+    public String toString() {
+        return "CartaoModel{" +
+                "id=" + id +
+                ", numero='" + numero + '\'' +
+                ", nomeTitular='" + nomeTitular + '\'' +
+                ", dataExpiracao='" + dataExpiracao + '\'' +
+                ", cvv=" + cvv +
+                '}';
+    }
+}
