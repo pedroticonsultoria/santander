893388877502a66d455f2a425ff644e958ad55b7
Pedro <palves.ct@petros.com>
2025-01-24 15:40:12 -0300

    Mostrando dados da tabela.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import dao.CartaoDAO;
+import model.CartaoModel;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CartaoDAO cartaoDAO = new CartaoDAO();
+
+        List<CartaoModel> cartoes = cartaoDAO.listarTodos();
+
+        if (cartoes.isEmpty()) {
+            System.out.println("Nenhum cartão encontrado.");
+        } else {
+            for (CartaoModel cartao : cartoes) {
+                System.out.println(cartao);
+            }
+        }
     }
 }
